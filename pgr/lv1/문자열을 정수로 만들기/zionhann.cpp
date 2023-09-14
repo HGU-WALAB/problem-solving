cd5f6f0512553c4aa9acc6fc6889cdbcf16da068
zionhann <hanzion1108@gmail.com>
2023-09-15 03:38:11 +0900

    PGR: 문자열을 정수로 만들기 (LV1)

---
+++ pgr/lv1/문자열을 정수로 만들기/zionhann.cpp
@@ -0,0 +1,15 @@
+/**
+ * @date 2023-09-15
+ * @author Sion Han
+ * @ref https://school.programmers.co.kr/learn/courses/30/lessons/12925
+ * @difficulty Lv. 1
+ * @details
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
